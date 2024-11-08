f1074f51e6875502caa0c73cb9155aa0ab0718aa
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2024-11-08 08:21:46 +0100

    Add files via upload

---
+++ news.php
@@ -0,0 +1,8 @@
+<?php
+// Postavljanje kolačića
+$vrijemeTrajanja = time() + (86400 * 30); // 86400 sekundi = 1 dan, kolačić će trajati 30 dana
+setcookie("korisnik", "Aleksandra Blažeković", $vrijemeTrajanja, "/"); // "/" znači da je kolačić dostupan na cijelom sajtu
+
+echo "Kolačić postavljen! Posjetite index.php za provjeru kolačića.";
+?>
+
