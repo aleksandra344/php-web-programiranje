cde0782d78b2094f413ca8b868494450346fb7cf
AleksandraBlažeković <93856657+aleksandra344@users.noreply.github.com>
2024-11-08 08:28:50 +0100

    Add files via upload

---
+++ news.php
@@ -0,0 +1,9 @@
+<?php
+// Inicijalizacija sesije
+session_start();
+
+// Postavljanje podataka u sesiju
+$_SESSION['korisnik'] = "Aleksandra Blažeković";
+
+echo "Podaci su spremljeni u sesiju! Posjetite index.php za provjeru sesijskih podataka.";
+?>
